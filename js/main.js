4fdee0d24f8844f91ec0b3294c98ee56c5a413d0
Eunice Dhivya <divi.seed@gmail.com>
2020-07-20 12:52:59 +0530

    add animation and click functions

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
+// var swup = new Swup();
+
 $(".mission-overview-btn").on("click", function(){
     $(".mission-home").css("display", "none")
     $(".mission-landing").css("display", "none")
-    $(".mission-overview").css("display", "grid")
+    $(".mission-overview").css("display", "grid").fadeIn("slow")
     $(".mission-objectives").css("display", "none")
     $("body").removeClass()
     $("body").addClass("mission-pg")
@@ -10,7 +12,7 @@ $(".mission-overview-btn").on("click", function(){
 $(".mission-landing-btn").on("click", function(){
     $(".mission-home").css("display", "none")
     $(".mission-overview").css("display", "none")
-    $(".mission-landing").css("display", "grid")
+    $(".mission-landing").css("display", "grid").fadeIn("slow")
     $(".mission-objectives").css("display", "none")
     $("body").removeClass()
     $("body").addClass("mission-landing-pg")
@@ -20,6 +22,11 @@ $(".mission-objectives-btn").on("click", function(){
     $(".mission-home").css("display", "none")
     $(".mission-overview").css("display", "none")
     $(".mission-landing").css("display", "none")
-    $(".mission-objectives").css("display", "grid")
+    $(".mission-objectives").css("display", "grid").fadeIn("slow")
+    $(".bgtext").addClass("rotatetxt")
+    $("body").removeClass()
+    $("body").addClass("mission-landing-pg")
     $(".bgtext").addClass("rotatetxt")
 })
+
+// $("#contact_form").fadeIn("slow");
